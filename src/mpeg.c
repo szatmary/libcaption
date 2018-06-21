9f76de67580ffa8aee759c1834aeed47212137d2
Matthew Szatmary <matt@szatmary.org>
2018-06-21 16:33:21 -0700

    dup control codes

--- src/mpeg.c
+++ src/mpeg.c
@@ -421,9 +421,11 @@ void sei_encode_eia608(sei_t* sei, cea708_t* cea708, uint16_t cc_data)
 
     if (0 == cea708->user_data.cc_count) { // This is a new 708 header, but a continuation of a 608 stream
         cea708_add_cc_data(cea708, 1, cc_type_ntsc_cc_field_1, eia608_control_command(eia608_control_resume_caption_loading, DEFAULT_CHANNEL));
+        cea708_add_cc_data(cea708, 1, cc_type_ntsc_cc_field_1, eia608_control_command(eia608_control_resume_caption_loading, DEFAULT_CHANNEL));
     }
 
     if (0 == cc_data) { // Finished
+        sei_encode_eia608(sei, cea708, eia608_control_command(eia608_control_end_of_caption, DEFAULT_CHANNEL));
         sei_encode_eia608(sei, cea708, eia608_control_command(eia608_control_end_of_caption, DEFAULT_CHANNEL));
         sei_append_708(sei, cea708);
         return;
